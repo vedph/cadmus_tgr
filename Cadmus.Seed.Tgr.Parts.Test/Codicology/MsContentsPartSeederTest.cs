9fec96e225c6e35e1502c30ec29fdbe944d8ce01
Myrmex <dfusi@hotmail.com>
2020-12-13 21:30:59 +0100

    MsContentsPart seeder test

---
+++ Cadmus.Seed.Tgr.Parts.Test/Codicology/MsContentsPartSeederTest.cs
@@ -0,0 +1,51 @@
+﻿using Cadmus.Core;
+using Cadmus.Seed.Tgr.Parts.Codicology;
+using Cadmus.Tgr.Parts.Codicology;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Tgr.Parts.Test.Codicology
+{
+    public sealed class MsContentsPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static MsContentsPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(MsContentsPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.tgr.ms-contents", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            MsContentsPartSeeder seeder = new MsContentsPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            MsContentsPart p = part as MsContentsPart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p);
+
+            Assert.NotEmpty(p.Contents);
+        }
+    }
+}
