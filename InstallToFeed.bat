d3fe48b6613c161341c53ac0139119ed6be2e80a
Myrmex <dfusi@hotmail.com>
2020-12-18 12:11:43 +0100

    Version numbers

--- InstallToFeed.bat
+++ InstallToFeed.bat
@@ -2,7 +2,7 @@
 echo PRESS ANY KEY TO INSTALL Cadmus Libraries TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
 pause
-c:\exe\nuget add .\Cadmus.Tgr.Parts\bin\Debug\Cadmus.Tgr.Parts.1.0.3.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Tgr.Parts\bin\Debug\Cadmus.Seed.Tgr.Parts.1.0.4.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Tgr.Services\bin\Debug\Cadmus.Tgr.Services.1.0.7.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Tgr.Parts\bin\Debug\Cadmus.Tgr.Parts.1.0.4.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Tgr.Parts\bin\Debug\Cadmus.Seed.Tgr.Parts.1.0.5.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Tgr.Services\bin\Debug\Cadmus.Tgr.Services.1.0.8.nupkg -source C:\Projects\_NuGet
 pause
