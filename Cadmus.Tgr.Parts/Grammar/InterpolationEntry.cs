748afef00ccffefe20d6bf1f54da88ba1efa9412
Myrmex <dfusi@hotmail.com>
2020-12-27 14:59:51 +0100

    InterpLayerFragment

---
+++ Cadmus.Tgr.Parts/Grammar/InterpolationEntry.cs
@@ -0,0 +1,77 @@
+﻿using Cadmus.Philology.Parts.Layers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadmus.Tgr.Parts.Grammar
+{
+    /// <summary>
+    /// An entry in an <see cref="InterpLayerFragment"/>.
+    /// </summary>
+    public class InterpolationEntry
+    {
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        public ApparatusEntryType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the language(s).
+        /// </summary>
+        public string[] Languages { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tag.
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the group identifier.
+        /// </summary>
+        public string GroupId { get; set; }
+
+        /// <summary>
+        /// Gets or sets an optional note.
+        /// </summary>
+        public string Note { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sources.
+        /// </summary>
+        public List<ReadingSource> Sources { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quotations.
+        /// </summary>
+        public List<VarQuotationEntry> Quotations { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InterpolationEntry"/>
+        /// class.
+        /// </summary>
+        public InterpolationEntry()
+        {
+            Sources = new List<ReadingSource>();
+            Quotations = new List<VarQuotationEntry>();
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[').Append(Type).Append(']');
+            if (Languages?.Length > 0) sb.Append(string.Join(", ", Languages));
+            sb.Append(": ").Append(Value);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Cadmus.Tgr.Parts/Grammar/ReadingSource.cs
@@ -0,0 +1,29 @@
+﻿namespace Cadmus.Tgr.Parts.Grammar
+{
+    /// <summary>
+    /// A source for a reading, used in <see cref="InterpLayerFragment"/>.
+    /// </summary>
+    public class ReadingSource
+    {
+        /// <summary>
+        /// Gets or sets the witness.
+        /// </summary>
+        public string Witness { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hand identifier.
+        /// </summary>
+        public string HandId { get; set; }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return Witness ?? base.ToString();
+        }
+    }
+}
