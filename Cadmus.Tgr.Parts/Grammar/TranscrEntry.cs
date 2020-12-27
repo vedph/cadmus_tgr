1e1fb2d5c76ae5e9ad8f1a0847fe6913ea0ad07b
Myrmex <dfusi@hotmail.com>
2020-12-27 16:38:09 +0100

    TranscrLayerFragment

---
+++ Cadmus.Tgr.Parts/Grammar/TranscrEntry.cs
@@ -0,0 +1,81 @@
+﻿using Cadmus.Philology.Parts.Layers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadmus.Tgr.Parts.Grammar
+{
+    /// <summary>
+    /// An entry in a <see cref="TranscrLayerFragment"/>.
+    /// </summary>
+    public class TranscrEntry
+    {
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        public ApparatusEntryType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the role.
+        /// </summary>
+        public string Role { get; set; }
+
+        /// <summary>
+        /// Gets or sets the language(s).
+        /// </summary>
+        public string[] Languages { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tag.
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the group identifier.
+        /// </summary>
+        public string GroupId { get; set; }
+
+        /// <summary>
+        /// Gets or sets an optional note.
+        /// </summary>
+        public string Note { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sources.
+        /// </summary>
+        public List<ReadingSource> Sources { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TranscrEntry"/> class.
+        /// </summary>
+        public TranscrEntry()
+        {
+            Sources = new List<ReadingSource>();
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[').Append(Type).Append(']');
+
+            if (!string.IsNullOrEmpty(Role))
+                sb.Append(' ').Append(Role).Append(' ');
+
+            if (Languages?.Length > 0)
+                sb.Append(' ').Append(string.Join(", ", Languages));
+
+            sb.Append(": ").Append(Value);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Cadmus.Tgr.Parts/Grammar/TranscrLayerFragment.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Fusi.Tools.Config;
+using Cadmus.Core.Layers;
+using Cadmus.Core;
+
+namespace Cadmus.Tgr.Parts.Grammar
+{
+    /// <summary>
+    /// Transcriptions layer fragment.
+    /// Tag: <c>fr.it.vedph.tgr.transcr</c>.
+    /// </summary>
+    /// <seealso cref="ITextLayerFragment" />
+    [Tag("fr.it.vedph.tgr.transcr")]
+    public sealed class TranscrLayerFragment : ITextLayerFragment
+    {
+        /// <summary>
+        /// Gets or sets the location of this fragment.
+        /// </summary>
+        /// <remarks>
+        /// The location can be expressed in different ways according to the
+        /// text coordinates system being adopted. For instance, it might be a
+        /// simple token-based coordinates system (e.g. 1.2=second token of
+        /// first block), or a more complex system like an XPath expression.
+        /// </remarks>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entries.
+        /// </summary>
+        public List<TranscrEntry> Entries { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TranscrLayerFragment"/>
+        /// class.
+        /// </summary>
+        public TranscrLayerFragment()
+        {
+            Entries = new List<TranscrEntry>();
+        }
+
+        /// <summary>
+        /// Get all the key=value pairs exposed by the implementor.
+        /// </summary>
+        /// <param name="item">The optional item. The item with its parts
+        /// can optionally be passed to this method for those parts requiring
+        /// to access further data.</param>
+        /// <returns>The pins: <c>fr.tag</c>=tag if any.</returns>
+        public IEnumerable<DataPin> GetDataPins(IItem item = null)
+        {
+            DataPinBuilder builder = new DataPinBuilder(
+                new StandardDataPinTextFilter());
+
+            // fr-tot-count
+            builder.Set(PartBase.FR_PREFIX + "tot", Entries?.Count ?? 0, false);
+
+            if (Entries?.Count > 0)
+            {
+                foreach (TranscrEntry entry in Entries)
+                {
+                    // fr-language
+                    if (entry.Languages?.Length > 0)
+                    {
+                        builder.AddValues(PartBase.FR_PREFIX + "language",
+                            entry.Languages);
+                    }
+                    // fr-value
+                    builder.AddValue(PartBase.FR_PREFIX + "value", entry.Value,
+                        filter: true);
+                    // fr-role
+                    builder.AddValue(PartBase.FR_PREFIX + "role", entry.Role);
+                    // fr-tag
+                    builder.AddValue(PartBase.FR_PREFIX + "tag", entry.Tag);
+                }
+            }
+
+            return builder.Build(null);
+        }
+
+        /// <summary>
+        /// Gets the definitions of data pins used by the implementor.
+        /// </summary>
+        /// <returns>Data pins definitions.</returns>
+        public IList<DataPinDefinition> GetDataPinDefinitions()
+        {
+            return new List<DataPinDefinition>(new[]
+            {
+                new DataPinDefinition(DataPinValueType.Integer,
+                    PartBase.FR_PREFIX + "tot-count",
+                    "The entries count."),
+                new DataPinDefinition(DataPinValueType.String,
+                    PartBase.FR_PREFIX + "language",
+                    "The list of languages.",
+                    "M"),
+                new DataPinDefinition(DataPinValueType.String,
+                    PartBase.FR_PREFIX + "value",
+                    "The list of values.",
+                    "MF"),
+                new DataPinDefinition(DataPinValueType.String,
+                    PartBase.FR_PREFIX + "tag",
+                    "The list of tags.",
+                    "M"),
+                new DataPinDefinition(DataPinValueType.String,
+                    PartBase.FR_PREFIX + "role",
+                    "The list of roles.",
+                    "M")
+            });
+        }
+
+        /// <summary>
+        /// Returns a <see cref="string" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (Entries?.Count > 0)
+            {
+                sb.Append(' ');
+                int n = 0;
+                foreach (var entry in Entries)
+                {
+                    if (++n > 3) break;
+                    if (n > 1) sb.Append("; ");
+                    sb.Append(entry);
+                }
+                if (Entries.Count > 3)
+                    sb.Append("...(").Append(Entries.Count).Append(')');
+            }
+
+            return sb.ToString();
+        }
+    }
+}
