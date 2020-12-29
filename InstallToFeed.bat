15362f12cce6b994b50cd0c328cb08b40b027cb5
Myrmex <dfusi@hotmail.com>
2020-12-29 18:20:24 +0100

    Updated packages

--- InstallToFeed.bat
+++ InstallToFeed.bat
@@ -4,5 +4,5 @@ echo Remember to generate the up-to-date package.
 pause
 c:\exe\nuget add .\Cadmus.Tgr.Parts\bin\Debug\Cadmus.Tgr.Parts.1.0.8.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Seed.Tgr.Parts\bin\Debug\Cadmus.Seed.Tgr.Parts.1.0.9.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Tgr.Services\bin\Debug\Cadmus.Tgr.Services.1.0.13.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Tgr.Services\bin\Debug\Cadmus.Tgr.Services.1.0.14.nupkg -source C:\Projects\_NuGet
 pause
