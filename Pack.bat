089736c04b08fd34418af2f1b20507ba68c169a3
Myrmex <dfusi@hotmail.com>
2021-03-28 12:02:32 +0200

    Updated packages

--- Pack.bat
+++ Pack.bat
@@ -1,6 +1,11 @@
 @echo off
 echo BUILD Cadmus Tgr Packages
 del .\Cadmus.Tgr.Parts\bin\Debug\*.nupkg
+del .\Cadmus.Tgr.Parts\bin\Debug\*.snupkg
+del .\Cadmus.Tgr.Services\bin\Debug\*.nupkg
+del .\Cadmus.Tgr.Services\bin\Debug\*.snupkg
+del .\Cadmus.Seed.Tgr.Parts\bin\Debug\*.nupkg
+del .\Cadmus.Seed.Tgr.Parts\bin\Debug\*.snupkg
 
 cd .\Cadmus.Tgr.Parts
 dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
