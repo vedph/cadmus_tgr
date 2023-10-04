6ec45d622841d46c90acf6834ab7c46ec851d492
Myrmex <dfusi@hotmail.com>
2023-10-04 11:49:45 +0200

    Updated packages

--- PushLocal.bat
+++ PushLocal.bat
@@ -1,7 +1,7 @@
 @echo off
 echo PRESS ANY KEY TO INSTALL Cadmus Libraries TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
-c:\exe\nuget add .\Cadmus.Tgr.Parts\bin\Debug\Cadmus.Tgr.Parts.6.1.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Tgr.Parts\bin\Debug\Cadmus.Seed.Tgr.Parts.6.1.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Tgr.Services\bin\Debug\Cadmus.Tgr.Services.6.1.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Tgr.Parts\bin\Debug\Cadmus.Tgr.Parts.6.1.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Tgr.Parts\bin\Debug\Cadmus.Seed.Tgr.Parts.6.1.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Tgr.Services\bin\Debug\Cadmus.Tgr.Services.6.1.2.nupkg -source C:\Projects\_NuGet
 pause
--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Components for the _Tradizione Grammaticale Romana_ project (here TGR for short)
 
 ## History
 
+### 6.1.2
+
+- 2023-10-04: updated packages.
+
 ### 6.1.1
 
 - 2023-09-19: updated packages.
